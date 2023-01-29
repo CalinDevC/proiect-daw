8698b3fdd13921cd7359ff5107912662a417050b
Calin <campean.calin@gmail.com>
2023-01-29 17:48:42 +0200

    updated contact form so it can write to database

--- shoppingcart/contact.php
+++ shoppingcart/contact.php
@@ -1,40 +1,50 @@
 <?php
 
-if (!empty($_POST)) {
-    $first_name = htmlspecialchars(addslashes($_POST['firstname']));
-    $last_name = htmlspecialchars(addslashes($_POST['last_name']));
-    $email = htmlspecialchars(addslashes($_POST['email']));
-    $subject = htmlspecialchars(addslashes($_POST['subject']));
+try {
+    // Connect to the database
+    $pdo = new PDO("mysql:host=localhost;dbname=magazin_daw", "root", "");
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $htmlMessage = '';
-    foreach($_POST as $key => $value) {
-        $htmlMessage .= "<p>$key: $value</p>";
+    if (!empty($_POST)) {
+        // Prepare the insert statement
+        $stmt = $pdo->prepare("INSERT INTO contact (first_name, last_name, email, subject) VALUES (:first_name, :last_name, :email, :subject)");
+
+        // Bind the form data to the insert statement
+        $stmt->bindParam(":first_name", $_POST['first_name']);
+        $stmt->bindParam(":last_name", $_POST['last_name']);
+        $stmt->bindParam(":email", $_POST['email']);
+        $stmt->bindParam(":subject", $_POST['subject']);
+
+        // Execute the insert statement
+        $stmt->execute();
+
+        // Redirect to the success page
+        header("Location: success.php");
+        exit();
     }
-
-    send_email($email, $htmlMessage);
-    // salveaza in baza de date!
-    /** ai tabelul CONTACT! */
-
+} catch (PDOException $e) {
+    // Output the error message
+    echo $e->getMessage();
 }
 
 ?>
-<link href="css/contact.css" rel="stylesheet" type="text/css">
+<link href="css/forms.css" rel="stylesheet" type="text/css">
 
 <div class="container">
 
-    <form action="index.php?page=contact">
-        <label for="fname">First Name</label>
-        <input type="text" id="fname" name="first_name" placeholder="Your name...">
+<form action="" method="post">
+    <label for="first_name">First Name:</label>
+    <input type="text" name="first_name" id="first_name" required>
 
-        <label for="lname">Last Name</label>
-        <input type="text" id="lname" name="last_name" placeholder="Your last name...">
+    <label for="last_name">Last Name:</label>
+    <input type="text" name="last_name" id="last_name" required>
 
-        <label for="email">Email</label>
-        <input type="email" id="email" name="email" placeholder="Your email...">
+    <label for="email">Email:</label>
+    <input type="email" name="email" id="email" required>
 
-        <label for="subject">Subject</label>
-        <textarea id="subject" name="subject" placeholder="Write something..." style="height:200px"></textarea>
+    <label for="subject">Subject</label>
+    <textarea id="subject" name="subject" placeholder="Write something..." style="height:200px"></textarea>
 
-        <input type="submit" value="Submit">
-    </form>
+    <input type="submit" value="Submit">
+</form>
 </div>
