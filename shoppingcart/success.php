8dcf2edd5b04c166801cfe49811596c25a33a591
Calin <campean.calin@gmail.com>
2023-01-31 00:17:55 +0200

    made success php FOR THE contact form and add proDUCT FORM

---
+++ shoppingcart/success.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Success</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f2f2f2;
+        }
+
+        .container {
+            width: 500px;
+            margin: 50px auto;
+            text-align: center;
+            padding: 50px;
+            background-color: white;
+            box-shadow: 0 0 10px #ccc;
+        }
+
+        h1 {
+            font-size: 36px;
+            color: green;
+            margin-bottom: 20px;
+        }
+
+        .return-button {
+            padding: 10px 20px;
+            border: 1px solid green;
+            border-radius: 5px;
+            background-color: green;
+            color: white;
+            text-decoration: none;
+            cursor: pointer;
+            margin-top: 20px;
+        }
+    </style>
+</head>
+<body>
+<div class="container">
+    <h1>Success!</h1>
+    <p>The operation was successful.</p>
+    <a class="return-button" href="javascript:history.back()">Return</a>
+</div>
+</body>
+</html>
